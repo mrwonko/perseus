a9c6ac77fd04bc9c949eeabe1222ceb3b29bd5cf
Willi Schinmeyer <willi@schinmeyer.de>
2015-05-10 11:19:35 +0200

    Moved template specialization code of instruction_pointer and code_segment into the cpp file

--- code/include/vm/code_segment.hpp
+++ code/include/vm/code_segment.hpp
@@ -44,59 +44,12 @@ namespace perseus
       template<>
       void push< opcode >( const opcode code )
       {
-        typedef std::underlying_type_t< opcode > type;
-        type value = static_cast< type >( code );
-
-        // 1 byte sequence
-        if( value < ( 1u << 7u ) )
-        {
-          push< unsigned char >( value );
-          return;
-        }
-
-        // multi byte sequence
-
-        // the compiler might just unroll these loops for us :)
-        // try each length, use the shortest one possible
-        for( unsigned int byte_count = 2; byte_count <= opcode_max_bytes; ++byte_count )
-        {
-          // CHAR_BIT bits in total, of which the marker uses one for the separating 0 and 1 for each continuation in the sequence
-          const unsigned int marker_bits = CHAR_BIT - byte_count;
-          const unsigned int continuation_count = byte_count - 1;
-          const unsigned int bits = marker_bits + continuation_count * CHAR_BIT;
-
-          // does the value fit in this length?
-          if( value < ( 1u << bits ) )
-          {
-            //    High Byte
-
-            unsigned char high_byte = value >> ( continuation_count * CHAR_BIT );
-
-            constexpr unsigned char high_bit_mask = 1u << ( CHAR_BIT - 1u );
-            // set the continuation_count highest bits to 1
-            for( unsigned char mask = high_bit_mask; mask > ( high_bit_mask >> continuation_count ); mask >>= 1 )
-            {
-              assert( !( high_byte & mask ) );
-              high_byte |= mask;
-            }
-            assert( !( high_byte & ( 0b10000000u >> continuation_count ) ) );
-            push< unsigned char >( high_byte );
-
-            //    Continuations
-            for( unsigned int continuation = 0u; continuation < continuation_count; ++continuation )
-            {
-              constexpr unsigned int char_mask = ( 1u << CHAR_BIT ) - 1u;
-              const unsigned int bytes_left = continuation_count - 1u - continuation;
-              const unsigned char byte = ( value >> ( bytes_left * CHAR_BIT ) ) & char_mask;
-              push< unsigned char >( byte );
-            }
-
-            //    done
-            return;
-          }
-        }
-        throw std::domain_error( "opcode too large to encode" );
+        pushOpcode( code );
       }
+
+    private:
+      /// implementation of push<opcode>()
+      void pushOpcode( const opcode code );
     };
   }
 }
--- code/include/vm/instruction_pointer.hpp
+++ code/include/vm/instruction_pointer.hpp
@@ -101,36 +101,12 @@ namespace perseus
       template<>
       opcode read< opcode >()
       {
-        typedef std::underlying_type_t< opcode > type;
-        type value = 0u;
-
-        const unsigned char first_byte = read< unsigned char >();
-        if( !( first_byte & ( 1u << 7u ) ) )
-        {
-          // single byte
-          value = first_byte;
-        }
-        else
-        {
-          // multi byte
-          unsigned int continuation_count = 1; // we only get here if the highest bit is 1
-          constexpr unsigned char high_bit_mask = 1u << ( CHAR_BIT - 1u );
-          for( unsigned char mask = high_bit_mask >> 1u; first_byte & mask; mask >>= 1u )
-          {
-            ++continuation_count;
-          }
-          // there are continuation_count 1-bits and one 0-bit
-          const unsigned char first_byte_mask = ( 1u << ( CHAR_BIT - 1u - continuation_count ) ) - 1u;
-          value = static_cast< type >( first_byte & first_byte_mask );
-          for( unsigned int continuation = 0u; continuation < continuation_count; ++continuation )
-          {
-            value <<= CHAR_BIT;
-            value |= static_cast< type >( read< unsigned char >() );
-          }
-        }
-
-        return static_cast< opcode >( value );
+        return readOpcode();
       }
+
+    private:
+      /// implementation of read<opcode>()
+      opcode readOpcode();
 
     private:
       /// Pointer to the code segment we point into
