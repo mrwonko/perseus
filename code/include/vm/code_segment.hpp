d984fc293fae1cc2ea6f4c5d34a760e474cdce48
Willi Schinmeyer <willi@schinmeyer.de>
2015-05-11 00:49:50 +0200

    bad_alloc notes on code_segment

--- code/include/vm/code_segment.hpp
+++ code/include/vm/code_segment.hpp
@@ -14,6 +14,8 @@ namespace perseus
   {
     /**
     @brief Memory segment containing executable bytecode.
+
+    @todo Add support for custom allocators? (remove mention of std::bad_alloc in push then)
     */
     class code_segment : public std::vector< char >
     {
@@ -25,6 +27,7 @@ namespace perseus
       @brief Writes a value to the end of the @ref code_segment
       @tparam T type of value to push back
       @param value the value to push back
+      @throws std::bad_alloc if the system is out of memory
       @todo Write bytes in reverse on big endian for cross-platform bytecode
       */
       template< typename T >
@@ -39,6 +42,7 @@ namespace perseus
 
       Opcodes have varying length; this function encodes the opcode properly, then stores it.
       @param code the opcode to encode and push back
+      @throws std::bad_alloc if the system is out of memory
       @note The encoding is somewhat similar to UTF-8 but without marking the continuations: The number of continuous high bits in the first byte is the number of additional bytes following. (UTF-8 has an additional 1-bit.) Those additional bytes are not marked in any way (unlike UTF-8, where they begin with `10xxxxxx`). So for example `10xxxxxx` `xxxxxxxx` for a 2 byte value, containing 8 to 14 bits.
       */
       template<>
