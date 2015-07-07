96ee8c383a08371c8c129ac5be553be18bb5949e
Willi Schinmeyer <willi@schinmeyer.de>
2015-07-07 23:01:35 +0200

    yet more utf8->utf32 tests

--- code/test/compiler/literals.cpp
+++ code/test/compiler/literals.cpp
@@ -1,4 +1,5 @@
 #include "compiler/conversions.hpp"
+#include "compiler/exceptions.hpp"
 #include "../u32string_ostream.hpp"
 
 #include <boost/test/unit_test.hpp>
@@ -44,6 +45,27 @@ BOOST_AUTO_TEST_CASE( three_bytes )
 BOOST_AUTO_TEST_CASE( four_bytes )
 {
   BOOST_CHECK_EQUAL( parse( u8"\"\U00010000\""s ), U"\U00010000"s );
+}
+
+BOOST_AUTO_TEST_CASE( stray_continuation )
+{
+  BOOST_CHECK_THROW( parse( "\"\x80\""s ), perseus::invalid_utf8 );
+}
+
+BOOST_AUTO_TEST_CASE( missing_continuation )
+{
+  BOOST_CHECK_THROW( parse( "\"\xC0_\""s ), perseus::invalid_utf8 );
+}
+
+BOOST_AUTO_TEST_CASE( too_large )
+{
+  BOOST_CHECK_THROW( parse( "\"\xf7\xbf\xbf\xbf\""s ), perseus::invalid_utf8 );
+}
+
+BOOST_AUTO_TEST_CASE( too_many_bytes )
+{
+  // 0xF9 = 0b1111'1001, 5 bytes
+  BOOST_CHECK_THROW( parse( "\"\xf9\x80\x80\x80\x80\""s ), perseus::invalid_utf8 );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
